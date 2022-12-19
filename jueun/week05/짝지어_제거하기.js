699ad3a2f895e89090024e63023b0de62f7fb53d
callmejeje <choije401@naver.com>
2022-12-19 20:49:14 +0900

    [PGS] 짝지어 제거하기 / 2단계 / ?

--- jueun/week05/짝지어_제거하기.js
+++ jueun/week05/짝지어_제거하기.js
@@ -28,22 +28,21 @@
 합계: 100.0 / 100.0
 */
 
-function solution(s)
-{
-    let left = [];
-    let right = s.split("");
-    let rightIdx = 0;
-    let sLength = s.length;
-    
-    for (let i = 0; i < sLength; i++) {
-        if (left.at(-1) === right[rightIdx]) {
-            left.pop();
-        } else {
-            left.push(right[rightIdx]);
-        }
-        rightIdx++;
+function solution(s) {
+  let left = [];
+  let right = s.split("");
+  let rightIdx = 0;
+  let sLength = s.length;
+
+  for (let i = 0; i < sLength; i++) {
+    if (left.at(-1) === right[rightIdx]) {
+      left.pop();
+    } else {
+      left.push(right[rightIdx]);
     }
-    
-    if (left.length > 0) return 0;
-    else return 1;
+    rightIdx++;
+  }
+
+  if (left.length > 0) return 0;
+  else return 1;
 }
