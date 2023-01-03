c22c0f8dbd24c297fb85dd38342c032352f23528
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-01-03 16:16:43 +0900

    코드 수정

--- haseung/Week3/Level1/명예의전당(1).js
+++ haseung/Week3/Level1/명예의전당(1).js
@@ -6,7 +6,7 @@ function solution(k, score) {
   for (let i = 0; i < score.length; i++) {
     if (i < k) honor.push(score[i]);
 
-    if (score[i] > min) {
+    if (score[i] > Math.min(...honor)) {
       honor.pop();
       honor.push(score[i]);
       honor.sort((a, b) => b - a);
