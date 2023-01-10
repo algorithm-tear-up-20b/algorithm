7e90f24843c96462ce624caf76c41e9a8db3818b
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-01-10 21:09:55 +0900

    stack 길이가 존재하지 않을 때 true를 리턴하도록 수정

--- haseung/Week4/Level2/괄호회전하기.js
+++ haseung/Week4/Level2/괄호회전하기.js
@@ -3,7 +3,6 @@ function solution(s) {
 
   const arr = s.split("");
   let result = 0;
-
   const isValid = (arr) => {
     const stack = [];
     for (let i = 0; i < arr.length; i++) {
@@ -14,10 +13,8 @@ function solution(s) {
         stack.pop();
       }
     }
-    if (stack.length) return false;
-    return true;
+    if (!stack.length) return true;
   };
-
   for (let i = 0; i < s.length; i++) {
     if (isValid(arr)) result++;
     arr.push(arr.shift());
@@ -26,20 +23,21 @@ function solution(s) {
 }
 
 /*정확성  테스트
-테스트 1 〉	통과 (4.86ms, 37MB)
-테스트 2 〉	통과 (3.45ms, 36.9MB)
-테스트 3 〉	통과 (4.25ms, 36.7MB)
-테스트 4 〉	통과 (4.18ms, 36.8MB)
-테스트 5 〉	통과 (9.28ms, 37.1MB)
-테스트 6 〉	통과 (4.59ms, 36.7MB)
-테스트 7 〉	통과 (7.25ms, 36.8MB)
-테스트 8 〉	통과 (7.66ms, 36.9MB)
-테스트 9 〉	통과 (13.50ms, 37.3MB)
-테스트 10 〉	통과 (28.37ms, 37.6MB)
-테스트 11 〉	통과 (30.39ms, 37.7MB)
-테스트 12 〉	통과 (0.10ms, 33.4MB)
-테스트 13 〉	통과 (0.10ms, 33.4MB)
-테스트 14 〉	통과 (0.09ms, 33.4MB)
+정확성  테스트
+테스트 1 〉	통과 (4.94ms, 37MB)
+테스트 2 〉	통과 (3.73ms, 36.9MB)
+테스트 3 〉	통과 (5.95ms, 36.7MB)
+테스트 4 〉	통과 (5.10ms, 36.8MB)
+테스트 5 〉	통과 (9.39ms, 37.3MB)
+테스트 6 〉	통과 (5.77ms, 36.8MB)
+테스트 7 〉	통과 (5.35ms, 36.9MB)
+테스트 8 〉	통과 (6.66ms, 37.1MB)
+테스트 9 〉	통과 (10.13ms, 37.4MB)
+테스트 10 〉	통과 (23.17ms, 37.6MB)
+테스트 11 〉	통과 (19.23ms, 37.8MB)
+테스트 12 〉	통과 (0.09ms, 33.7MB)
+테스트 13 〉	통과 (0.10ms, 33.5MB)
+테스트 14 〉	통과 (0.10ms, 33.6MB)
 채점 결과
 정확성: 100.0
 합계: 100.0 / 100.0*/
