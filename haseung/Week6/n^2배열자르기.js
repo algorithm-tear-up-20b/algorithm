51799364175531f8da436197e2131edba10726d4
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-01-30 19:05:49 +0900

    Chore : 주석 추가, 메소드 변경 - parseInt -> Math.floor로 변경

--- haseung/Week6/n^2배열자르기.js
+++ haseung/Week6/n^2배열자르기.js
@@ -8,16 +8,40 @@ function solution(n, left, right) {
   const result = [];
 
   for (let i = left; i <= right; i++) {
-    const row = parseInt(i / n); //y
+    const row = Math.floor(i / n); //y
     const col = i % n; //x
     result.push(Math.max(row, col) + 1);
   }
   return result;
 }
 
-//30분
-
 //row col 0 2 result [3]
 //row col 1 0 result [3,2]
 //row col 1 1 result [3,2,2]
 //row col 1 2 result [3,2,2,3]
+
+/* 정확성  테스트
+정확성  테스트
+테스트 1 〉	통과 (4.71ms, 43.6MB)
+테스트 2 〉	통과 (4.92ms, 49.5MB)
+테스트 3 〉	통과 (4.89ms, 49.7MB)
+테스트 4 〉	통과 (0.13ms, 33.5MB)
+테스트 5 〉	통과 (0.12ms, 33.5MB)
+테스트 6 〉	통과 (4.91ms, 43.7MB)
+테스트 7 〉	통과 (4.98ms, 43.7MB)
+테스트 8 〉	통과 (4.89ms, 43.4MB)
+테스트 9 〉	통과 (4.85ms, 44.3MB)
+테스트 10 〉	통과 (4.88ms, 44.4MB)
+테스트 11 〉	통과 (6.51ms, 44.1MB)
+테스트 12 〉	통과 (5.21ms, 47.1MB)
+테스트 13 〉	통과 (5.55ms, 47.6MB)
+테스트 14 〉	통과 (5.17ms, 44.4MB)
+테스트 15 〉	통과 (5.11ms, 49.6MB)
+테스트 16 〉	통과 (5.07ms, 50MB)
+테스트 17 〉	통과 (6.94ms, 44.6MB)
+테스트 18 〉	통과 (5.25ms, 49.6MB)
+테스트 19 〉	통과 (5.79ms, 49.8MB)
+테스트 20 〉	통과 (5.03ms, 43.5MB)
+채점 결과
+정확성: 100.0
+합계: 100.0 / 100.0 */
