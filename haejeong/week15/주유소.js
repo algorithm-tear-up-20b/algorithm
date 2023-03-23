9a436879d619266d290dc62cc46d3b33bab04b6e
Haejeong <skhj5822@naver.com>
2023-03-23 11:16:59 +0900

    [BOJ] 주유소 / 실버3 / 50m

--- haejeong/week15/주유소.js
+++ haejeong/week15/주유소.js
@@ -1,19 +1,19 @@
-const N = 4;
-const distance = [2, 3, 1];
-const price = [5, 2, 4, 1];
-price.pop();
+const input = ["4", "2 3 1", "5 2 4 1"];
+// const input = ["4", "3 3 4", "1 1 1 1"];
+// const input = ["4", "2 2 2", "2 4 1 2"];
 
-const solution = (N, distance, price) => {
-  let answer = 0;
-  for (let i = 0; i < N; i++) {
-    if (price[i] === Math.min(...price)) {
-      answer += price[i] * distance.slice(i).reduce((acc, cur) => acc + cur, 0);
-      break;
-    } else {
-      answer += price[i] * distance[i];
-    }
+const [N, km, prices] = [input[0], input[1].split(" ").map(BigInt), input[2].split(" ").map(BigInt)];
+
+const solution = (prices, km) => {
+  let minPrice = prices[0];
+  let cost = 0n;
+
+  for (let i = 0; i < km.length; i++) {
+    if (minPrice > prices[i]) minPrice = prices[i];
+    cost += minPrice * km[i];
   }
-  return answer;
+
+  return String(cost);
 };
 
-console.log(solution(N, distance, price));
+console.log(solution(prices, km));
